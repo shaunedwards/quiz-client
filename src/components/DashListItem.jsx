86a29c56fc318ef469c65c001c784ab6f2c10c85
Shaun <sme4@aber.ac.uk>
2020-03-14 19:47:52 +0000

    add dashboard components

---
+++ src/components/DashListItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Typography,
+  Button
+} from '@material-ui/core';
+import { KeyboardArrowRight } from '@material-ui/icons';
+
+function DashListItem(props) {
+  const { title, data, more } = props;
+  return (
+    <List style={{marginRight:30}}>
+      <Typography variant="h6" style={{fontWeight:700}}>
+        {title}
+      </Typography>
+      {data && data.length > 0 ? data.map(item => (
+        <ListItem divider button component="a" href={`/quiz/${item._id}`} key={item._id}>
+          <ListItemText primary={item.title} />
+          <ListItemIcon>
+            <KeyboardArrowRight />
+          </ListItemIcon>
+        </ListItem>
+      )): <Typography component="p">Your list is currently empty</Typography>}
+      {data && data.length > 0 ? <Button fullWidth size="small" href={more}>View more...</Button> : null}
+    </List>
+  )
+}
+
+export default DashListItem;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { 
+  Grid,
+  Typography
+} from '@material-ui/core';
 
 import Header from './Header';
+import DashListItem from './DashListItem';
 
 function Dashboard(props) {
+  const [games, setGames] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+
+  async function getUserData(endpoint) {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/users/${endpoint}`, { credentials: 'include' });
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const [games, favourites] = await Promise.all([
+        getUserData('games'),
+        getUserData('favourites')
+      ]);
+      setGames(games);
+      setFavourites(favourites);
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header title="Dashboard" />
-      <Grid container style={{paddingLeft: 30, paddingTop: 10}}>
-        <h1>{`Hi, ${props.user.name}!`}</h1>
+      <Grid container alignItems="flex-start" justify="flex-end" direction="row" style={{paddingRight:30, paddingTop:25}}>
+        <Typography component="p">
+          Logged in as: <span style={{fontWeight:700}}>{props.user.uid}</span>
+        </Typography>
+      </Grid>
+      <Grid container alignItems="flex-start" justify="flex-start" direction="row" style={{paddingLeft:30, paddingTop:10}}>
+        <Grid item xs={12} md={4}>
+          <DashListItem title="Your Games" more="/dashboard/games" data={games} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <DashListItem title="Your Favourites" more="/dashboard/favourites" data={favourites} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <DashListItem title="Recently Published" more="/discover" data={null} />
+        </Grid>
       </Grid>
     </>
   )
