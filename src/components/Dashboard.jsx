0aeec66446e9ff111dee7fc303ee98e093fe4155
Shaun <sme4@aber.ac.uk>
2020-03-14 20:38:50 +0000

    add recently published games

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,21 +9,30 @@ import DashListItem from './DashListItem';
 
 function Dashboard(props) {
   const [games, setGames] = useState([]);
+  const [recent, setRecent] = useState([]);
   const [favourites, setFavourites] = useState([]);
 
   async function getUserData(endpoint) {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/users/${endpoint}`, { credentials: 'include' });
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/users/${endpoint}?limit=5`, { credentials: 'include' });
+    const data = await response.json();
+    return data;
+  }
+
+  async function getRecentGames() {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/games/recent`);
     const data = await response.json();
     return data;
   }
 
   useEffect(() => {
     async function fetchData() {
-      const [games, favourites] = await Promise.all([
+      const [games, favourites, recent] = await Promise.all([
         getUserData('games'),
-        getUserData('favourites')
+        getUserData('favourites'),
+        getRecentGames()
       ]);
       setGames(games);
+      setRecent(recent);
       setFavourites(favourites);
     }
     fetchData();
@@ -45,7 +54,7 @@ function Dashboard(props) {
           <DashListItem title="Your Favourites" more="/dashboard/favourites" data={favourites} />
         </Grid>
         <Grid item xs={12} md={4}>
-          <DashListItem title="Recently Published" more="/discover" data={null} />
+          <DashListItem title="Recently Published" more="/discover" data={recent} />
         </Grid>
       </Grid>
     </>
